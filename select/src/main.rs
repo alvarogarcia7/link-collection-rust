06b7795439b1d4860096666aba8dec6d2ea09e57
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-10 23:36:20 +0400

    test the ls subcommand

--- select/src/main.rs
+++ select/src/main.rs
@@ -24,9 +24,26 @@ fn run() -> Result<(), ()> {
     Ok(())
 }
 
-fn main() {
+pub fn main() {
     match run() {
         Ok(()) => (),
         Err(e) => println!("{:?}", e),
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use crate::cli;
+
+    #[test]
+    pub fn parse_the_ls_subcommand() {
+        let arg_vec = vec!["", "ls", "$FILE"];
+
+        let actual = cli().get_matches_from(arg_vec);
+        println!("{:?}", actual);
+
+        assert_eq!(actual.subcommand().unwrap().0, "ls");
+        assert_eq!(actual.subcommand().unwrap().1.ids().len(), 1);
+        assert!(actual.subcommand().unwrap().1.contains_id("FILE"));
     }
 }
