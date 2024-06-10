69264e0b6fb308f4cdc95a68ae7931f6d5317c4f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-10 19:01:01 +0400

    read file format using rrecutils

--- data_access/src/lib.rs
+++ data_access/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
-mod tests {
-    use super::*;
+pub mod tests {
+    use rrecutils::Recfile;
+    use std::env;
+    use std::fs::File;
+    use std::io::BufReader;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn read_file() {
+        println!("{:?}", env::current_dir());
+        let file = File::open("./data/links.rec").unwrap();
+        let reader = BufReader::new(file);
+
+        let records = Recfile::parse(reader).unwrap().records;
+
+        println!("{:?}", records);
+
+        assert_eq!(records.len(), 2);
+        assert_eq!(records[0].rec_type, None);
+        assert_eq!(records[1].rec_type, Some("Link".to_string()));
     }
 }
