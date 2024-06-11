ed15a24bd168368d10964b24c3c77895c0ec285c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-11 19:13:58 +0400

    [6.1] Use clap for top command and multiple subcommands - copy cookbook

--- select/src/bin/top.rs
+++ select/src/bin/top.rs
@@ -1,14 +1,172 @@
 extern crate core;
 
 use core::str;
+// Source: https://docs.rs/clap/latest/clap/_derive/_cookbook/git_derive/index.html
+use std::ffi::OsStr;
+use std::ffi::OsString;
+use std::path::PathBuf;
 
+use clap::arg;
 use clap::builder::Str;
-use clap::{arg, Command};
+use clap::{Args, Parser, Subcommand, ValueEnum};
 
-use data_access::recutils_database::RecutilsDatabaseAccess;
-use domain::Record;
-use select::commands::list_command;
-use select::commands::list_command::MyError;
+/// A fictional versioning CLI
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "git")]
+#[command(about = "A fictional versioning CLI", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand, PartialEq)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    List {
+        file: String,
+    },
+    /// Compare two commits
+    Diff {
+        #[arg(value_name = "COMMIT")]
+        base: Option<OsString>,
+        #[arg(value_name = "COMMIT")]
+        head: Option<OsString>,
+        #[arg(last = true)]
+        path: Option<OsString>,
+        #[arg(
+            long,
+            require_equals = true,
+            value_name = "WHEN",
+            num_args = 0..=1,
+            default_value_t = ColorWhen::Auto,
+            default_missing_value = "always",
+            value_enum
+        )]
+        color: ColorWhen,
+    },
+    /// pushes things
+    #[command(arg_required_else_help = true)]
+    Push {
+        /// The remote to target
+        remote: String,
+    },
+    /// adds things
+    #[command(arg_required_else_help = true)]
+    Add {
+        /// Stuff to add
+        #[arg(required = true)]
+        path: Vec<PathBuf>,
+    },
+    Stash(StashArgs),
+    #[command(external_subcommand)]
+    External(Vec<OsString>),
+}
+
+#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+enum ColorWhen {
+    Always,
+    Auto,
+    Never,
+}
+
+impl std::fmt::Display for ColorWhen {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.to_possible_value()
+            .expect("no values are skipped")
+            .get_name()
+            .fmt(f)
+    }
+}
+
+#[derive(Debug, Args, PartialEq)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+struct StashArgs {
+    #[command(subcommand)]
+    command: Option<StashCommands>,
+
+    #[command(flatten)]
+    push: StashPushArgs,
+}
+
+#[derive(Debug, Subcommand, PartialEq)]
+enum StashCommands {
+    Push(StashPushArgs),
+    Pop { stash: Option<String> },
+    Apply { stash: Option<String> },
+}
+
+#[derive(Debug, Args, PartialEq)]
+struct StashPushArgs {
+    #[arg(short, long)]
+    message: Option<String>,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::List { file } => {
+            println!("Cloning {file}");
+        }
+        Commands::Diff {
+            mut base,
+            mut head,
+            mut path,
+            color,
+        } => {
+            if path.is_none() {
+                path = head;
+                head = None;
+                if path.is_none() {
+                    path = base;
+                    base = None;
+                }
+            }
+            let base = base
+                .as_deref()
+                .map(|s| s.to_str().unwrap())
+                .unwrap_or("stage");
+            let head = head
+                .as_deref()
+                .map(|s| s.to_str().unwrap())
+                .unwrap_or("worktree");
+            let path = path.as_deref().unwrap_or_else(|| OsStr::new(""));
+            println!(
+                "Diffing {}..{} {} (color={})",
+                base,
+                head,
+                path.to_string_lossy(),
+                color
+            );
+        }
+        Commands::Push { remote } => {
+            println!("Pushing to {remote}");
+        }
+        Commands::Add { path } => {
+            println!("Adding {path:?}");
+        }
+        Commands::Stash(stash) => {
+            let stash_cmd = stash.command.unwrap_or(StashCommands::Push(stash.push));
+            match stash_cmd {
+                StashCommands::Push(push) => {
+                    println!("Pushing {push:?}");
+                }
+                StashCommands::Pop { stash } => {
+                    println!("Popping {stash:?}");
+                }
+                StashCommands::Apply { stash } => {
+                    println!("Applying {stash:?}");
+                }
+            }
+        }
+        Commands::External(args) => {
+            println!("Calling out to {:?} with {:?}", &args[0], &args[1..]);
+        }
+    }
+
+    // Continued program logic goes here...
+}
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum SubcommandType {
@@ -82,69 +240,41 @@ impl From<&str> for SubcommandType {
     }
 }
 
-fn cli() -> Command {
-    Command::new("link-collection")
-        .about("A tool to manage your links")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        // .allow_external_subcommands(true)
-        .subcommand(
-            Command::new(SubcommandType::List)
-                .about("Reads a file")
-                .arg(
-                    arg!(<FILE> "The database file to read")
-                        // .action(ArgAction::Set)
-                        .required(true),
-                )
-                .arg_required_else_help(true),
-        )
-}
-
-fn run() -> Result<Vec<Record>, MyError> {
-    let matches = cli().get_matches();
-
-    match matches
-        .subcommand()
-        .map(|(f, rest)| (SubcommandType::from(f), rest))
-    {
-        Some((SubcommandType::List, arg_matches)) => {
-            let path = arg_matches.get_one::<String>("FILE");
-            println!("Reading database file at: {:?}", path);
-            let access = RecutilsDatabaseAccess::new(path.unwrap(), "Link".to_string());
-            list_command::run(access)
-        }
-        _ => todo!(),
-    }
-}
-
-pub fn main() {
-    run().unwrap();
-    // match
-    // {
-    //     Ok(_) => (),
-    //     Err(e) => println!("{:?}", e),
-    // }
-}
+// fn run() -> Result<Vec<Record>, MyError> {
+//     let matches = Cli::parse().get_matches();
+//
+//     match matches
+//         .subcommand()
+//         .map(|(f, rest)| (SubcommandType::from(f), rest))
+//     {
+//         Some((SubcommandType::List, arg_matches)) => {
+//             let path = arg_matches.get_one::<String>("FILE");
+//             println!("Reading database file at: {:?}", path);
+//             let access = RecutilsDatabaseAccess::new(path.unwrap(), "Link".to_string());
+//             list_command::run(access)
+//         }
+//         _ => todo!(),
+//     }
+// }
 
 #[cfg(test)]
 pub mod tests {
-    use crate::cli;
+    use clap::Parser;
+
+    use crate::{Cli, Commands};
 
     #[test]
     pub fn parse_the_ls_subcommand() {
-        let arg_vec = vec!["", "ls", "$FILE"];
+        let arg_vec = ["", "list", "$FILE"];
 
-        let actual = cli().get_matches_from(arg_vec);
+        let actual = Cli::parse_from(arg_vec.iter());
         println!("{:?}", actual);
 
-        assert_eq!(actual.subcommand().unwrap().0, "ls");
-
-        let subcommand_args = actual.subcommand().unwrap().1;
-        assert_eq!(subcommand_args.ids().len(), 1);
-        assert!(subcommand_args.contains_id("FILE"));
         assert_eq!(
-            subcommand_args.get_one::<String>("FILE"),
-            Some(&"$FILE".to_string())
+            actual.command,
+            Commands::List {
+                file: "$FILE".to_string()
+            }
         );
     }
 }
