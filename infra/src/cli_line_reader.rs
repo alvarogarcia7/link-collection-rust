479e1b1bd4a64db8627ba8ec0ee738c1f61bfb5c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-12 13:10:19 +0400

    [2.1] more fields to be read from the CLI

--- infra/src/cli_line_reader.rs
+++ infra/src/cli_line_reader.rs
@@ -99,13 +99,10 @@ impl RecordProvider for CliReaderRecordProvider {
     fn fetch(&mut self) -> Record {
         let id = "a1a6925a-7958-11e8-a87f-0242ac110002".to_string();
         let formatted_date = "Tue, 26 Jun 2018 15:50:21 +0000".to_string();
-        let category = "category1".to_string();
-        let formatted_tags = "tag1, tag_2, name-surname".to_string();
 
         let fields_dto = vec![
             ("Id".to_string(), id),
             ("Date".to_string(), formatted_date),
-            ("Category".to_string(), category),
             (
                 "Title".to_string(),
                 self.line_reader.read_line("Title (mandatory)".to_string()),
@@ -116,7 +113,12 @@ impl RecordProvider for CliReaderRecordProvider {
                     .read_until_ctrl_d("Body".to_string())
                     .join("\n"),
             ),
-            ("Tags".to_string(), formatted_tags),
+            (
+                "Category".to_string(),
+                self.line_reader
+                    .read_line("Category (mandatory)".to_string()),
+            ),
+            ("Tags".to_string(), "tag1, tag_2, name-surname".to_string()),
         ];
 
         let mut fields: HashMap<String, String> = HashMap::with_capacity(fields_dto.len());
