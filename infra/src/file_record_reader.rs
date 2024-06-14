8efd2c9ea0fdcfcd79aec82e60cecf6f7d03f4e2
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-14 10:17:13 +0400

    cleaner format for data

--- infra/src/file_record_reader.rs
+++ infra/src/file_record_reader.rs
@@ -23,28 +23,24 @@ impl RecordProvider for FileReaderRecordProvider {
             self.path
         );
 
-        let fields_dto = vec![
-            (
-                "Id".to_string(),
-                "a1a6925a-7958-11e8-a87f-0242ac110002".to_string(),
-            ),
-            (
-                "Date".to_string(),
-                "Tue, 26 Jun 2018 15:50:21 +0000".to_string(),
-            ),
-            ("Category".to_string(), "category1".to_string()),
-            ("Title".to_string(), "Good title".to_string()),
-            (
-                "Body".to_string(),
-                ["Body Line1", "Body line 2"].join("\n+"),
-            ),
-            ("Tags".to_string(), "tag1, tag_2, name-surname".to_string()),
-        ];
-        let mut fields: HashMap<String, String> = HashMap::with_capacity(fields_dto.len());
+        let body = ["Body Line1", "Body line 2"].join("\n+");
+        let fields_dto: HashMap<&str, &str> = HashMap::from([
+            ("Id", "a1a6925a-7958-11e8-a87f-0242ac110002"),
+            ("Date", "Tue, 26 Jun 2018 15:50:21 +0000"),
+            ("Category", "category1"),
+            ("Title", "Good title"),
+            ("Body", body.as_str()),
+            ("Tags", "tag1, tag_2, name-surname"),
+        ]);
 
-        for (key, value) in fields_dto.iter() {
-            fields.insert(key.clone(), value.clone());
-        }
+        let fields = fields_dto
+            .iter()
+            .map(|(k, v)| (k.to_string(), v.to_string()))
+            .collect();
+        let fields_dto = fields_dto
+            .iter()
+            .map(|(k, v)| (k.to_string(), v.to_string()))
+            .collect();
 
         Record {
             record_type: "Link".to_string(),
