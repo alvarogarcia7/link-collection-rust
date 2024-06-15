659b5992a6ec95867554e5543b63179d7cdbe446
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-15 20:25:01 +0400

    [4.2] Parse the fields from the HN response

--- downloader/src/lib.rs
+++ downloader/src/lib.rs
@@ -1,17 +1,63 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct NodeView {
+    pub id: u64,
+    pub by: String,
+    pub time: u64,
+    pub title: String,
+    pub url: String,
+}
+
+impl NodeView {}
+
+struct FirebaseHackerNewsDownloader {
+    domain: String,
+}
+impl FirebaseHackerNewsDownloader {
+    fn new(domain: String) -> Self {
+        FirebaseHackerNewsDownloader { domain }
+    }
+
+    fn get_item(&self, id: u64) -> Result<NodeView, reqwest::Error> {
+        let response = reqwest::blocking::Client::new();
+        let path = format!("{}/v0/item/{}.json", self.domain, id);
+        let response = response
+            .get(path)
+            // .header(reqwest::header::USER_AGENT, "My Rust Application")
+            .send()?;
+        response.json::<NodeView>()
+    }
+}
+
+impl Default for FirebaseHackerNewsDownloader {
+    fn default() -> Self {
+        FirebaseHackerNewsDownloader {
+            domain: "https://hacker-news.firebaseio.com".to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
 pub mod tests {
-    use reqwest::{header, Error};
+    use super::*;
 
     #[test]
-    fn main() -> Result<(), Error> {
-        let response = reqwest::blocking::Client::new();
-        let response = response
-            .get("http://0.0.0.0:8181/api/v1/reservations/1")
-            .header(header::USER_AGENT, "My Rust Application")
-            .send()?
-            .text()?;
+    fn fetch_and_parse_the_fields() {
+        let downloader = FirebaseHackerNewsDownloader::new("http://0.0.0.0:8181".to_string());
 
-        println!("{}", response);
+        let response = downloader.get_item(12339182329);
 
-        Ok(())
+        assert!(response.is_ok());
+
+        let response = response.unwrap();
+        assert_eq!(response.by, "vanusa");
+        assert_eq!(response.time, 1621276965);
+        assert_eq!(
+            response.title,
+            "Why Is the Gaza Strip Blurry on Google Maps?"
+        );
+
+        assert_eq!(response.url, "https://www.bbc.com/news/57102499");
     }
 }
