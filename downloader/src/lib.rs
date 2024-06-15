45798f4731ec9a7e21a7dea10349430e79be3a13
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-15 19:58:17 +0400

    [4.2] Isue a simple request to a http server

--- downloader/src/lib.rs
+++ downloader/src/lib.rs
@@ -1,1 +1,17 @@
+pub mod tests {
+    use reqwest::{header, Error};
 
+    #[test]
+    fn main() -> Result<(), Error> {
+        let response = reqwest::blocking::Client::new();
+        let response = response
+            .get("http://0.0.0.0:8181/api/v1/reservations/1")
+            .header(header::USER_AGENT, "My Rust Application")
+            .send()?
+            .text()?;
+
+        println!("{}", response);
+
+        Ok(())
+    }
+}
